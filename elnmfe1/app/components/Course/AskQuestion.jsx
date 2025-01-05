963b53bf437bfcecea0d707c5e38f7e7e3efacf5
den3110 <datistpham@gmail.com>
2025-01-06 01:19:18 +0700

    commit

--- elnmfe1/app/components/Course/AskQuestion.jsx
+++ elnmfe1/app/components/Course/AskQuestion.jsx
@@ -33,7 +33,7 @@ const ChatUI = () => {
           {
             headers: {
               "Content-Type": "application/json",
-              Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+              Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
             }
           }
         )
